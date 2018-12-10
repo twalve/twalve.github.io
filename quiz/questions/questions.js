a6b40aa23e47e63a5e7d547b8a74e1e263958abe
Sharkie <craig.sharkie@gmail.com>
2018-12-11 06:00:10 +1100

    Adding foundation quiz files

---
+++ quiz/questions/questions.js
@@ -0,0 +1,52 @@
+const questions = {
+  q0: {
+    question: "Pick the valid jQuery",
+    answers: [
+      "$().hide()",
+      "$(\"main\").hide()",
+      "$(main).hide()",
+      "Either B or C"
+    ],
+    answer: 4
+  },
+  q1: {
+    question: "Pick the valid CSS selector",
+    answers: [
+      "#1st",
+      ".first.heading",
+      "#first#heading",
+      "#.first"
+    ],
+    answer: 2
+  },
+  q2: {
+    question: "Pick the valid HTML",
+    answers: [
+      "<p>paragraph<p>",
+      "<p>paragraph</p>",
+      "<p><a>paragraph</p></a>",
+      "Either A or B",
+    ],
+    answer: 4
+  },
+  q3: {
+    question: "Pick the valid JavaScript",
+    answers: [
+      "var temperature = \"hot\";",
+      "let temperature = \"scoped\";",
+      "const temperature = \"reliable\";",
+      "All of the above",
+    ],
+    answer: 4
+  },
+  q4: {
+    question: "Pick the valid declaration",
+    answers: [
+      "var temperature",
+      "let temperature",
+      "var temperature.current",
+      "Either A or B",
+    ],
+    answer: 1
+  }
+}
