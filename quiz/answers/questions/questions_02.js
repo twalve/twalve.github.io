cafdaf57607b642421f3d373506338795f0a8dca
Sharkie <craig.sharkie@gmail.com>
2018-12-17 04:58:22 +1100

    Foundation for Answers

---
+++ quiz/answers/questions/questions_02.js
@@ -0,0 +1,84 @@
+const questions = {
+  q0: {
+    question: "Which of these declares a function",
+    answers: [
+      "function(){}",
+      "function myFunction(){}",
+      "var otherFunction = function(){}",
+      "All of the above"
+    ],
+    answer: 4
+  },
+  q1: {
+    question: "Useful <form> element attributes",
+    answers: [
+      "action=\"collector\"",
+      "for=\"profile\"",
+      "method=\"POST\"",
+      "Not B"
+    ],
+    answer: 4
+  },
+  q2: {
+    question: "Which are the grouped radio buttons",
+    answers: [
+      "<input type=radio value=true name=boolean>",
+      "<input type=radio value=false name=boolean>",
+      "<input type=radio value=maybe name=boo1ean>",
+      "A and B",
+    ],
+    answer: 4
+  },
+  q3: {
+    question: "Which inputs would validate",
+    answers: [
+      "<input type=text required>",
+      "<input type=text required='true'>",
+      "<input type=text required=\"required\">",
+      "<input type=text required='required'>",
+      "Not B",
+    ],
+    answer: 5
+  },
+  q4: {
+    question: "Pick the valid declarations",
+    answers: [
+      "var temperature",
+      "let Temperature",
+      "const TEMPARATURE",
+      "All of the above",
+    ],
+    answer: 4
+  },
+  q5: {
+    question: "Which is your favourite FEWD lecturer",
+    answers: [
+      "Sharkie",
+      "Gates",
+      "Jobs",
+      "B or C? Really? NO",
+    ],
+    answer: 1
+  },
+  q6: {
+    question: "Which week of course FEWDR1 are we completing",
+    answers: [
+      "\"7\"",
+      "Answer to the Ultimate Question of Life, the Universe, and Everything",
+      "7",
+      "A or C, if you coerce them",
+    ],
+    answer: 4
+  },
+  q7: {
+    question: "Which of these elements should be unique",
+    answers: [
+      "<html>",
+      "<head>",
+      "<body>",
+      "<main>",
+      "All of the above",
+    ],
+    answer: 5
+  }
+}
