bf07a2cd4d48ad127273b3cb6f77a021a12349db
Sharkie <craig.sharkie@gmail.com>
2018-12-18 21:30:44 +1100

    Adding yet-another block of questions

---
+++ quiz/questions/questions/questions_2018_50.js
@@ -0,0 +1,152 @@
+,
+q40: {
+  _question: `// What will the code below output to the console?
+
+  console.log(0.1 + 0.2);
+  console.log(0.1 + 0.2 == 0.3);
+  `,
+  answers: [
+    "0.3, 0.3",
+    "0.3, true",
+    "0.3, false",
+    "0.3, 0.1false",
+  ],
+  answer: 2,
+  author: "toptal",
+  level: 5
+}
+},
+q41: {
+  _question: `// Consider the two functions below. Will they both return the same thing?
+
+  function foo1()
+  {
+    return {
+        bar: "hello"
+    };
+  }
+
+  function foo2()
+  {
+    return
+    {
+        bar: "hello"
+    };
+  }
+
+  `,
+  answers: [
+    "Yes",
+    "No",
+    "True",
+    "False",
+  ],
+  answer: 1,
+  author: "toptal",
+  level: 5
+}
+},
+q42: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q43: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q44: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q45: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q46: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q47: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q48: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+},
+q49: {
+  _question: ``,
+  answers: [
+    "",
+    "",
+    "",
+    "",
+  ],
+  answer: ,
+  author: "",
+  level: 5
+}
+}
