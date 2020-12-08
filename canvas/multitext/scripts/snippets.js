0fc28e2c9e5848e6af120e27de97ae8da369e034
Sharkie <craig.sharkie@gmail.com>
2020-12-09 08:52:33 +1100

    C5N Canvas.multitext()
    INITIAL

---
+++ canvas/multitext/scripts/snippets.js
@@ -0,0 +1,27 @@
+const snippets = {
+  bold: `Point your remote at the TV and press {BOLD|TEXT:Volume +}`,
+  icon: `Green  {ICON|URL:../images/buttons/buttonGreen.png}  for Yes | Red  {ICON|URL:../images/buttons/buttonRed.png}  for No`,
+  styledIcon: `{ICON|URL:../images/buttons/buttonGreen.png;MARGIN_BOTTOM:-2;MARGIN_LEFT:6;MARGIN_RIGHT:2;}  Go to Top`,
+  rgyb: `{ICON|URL:../images/buttons/buttonRed.png} {ICON|URL:../images/buttons/buttonGreen.png} {ICON|URL:../images/buttons/buttonYellow.png} {ICON|URL:../images/buttons/buttonBlue.png}`,
+  multi: `Don't Worry.\nPairing can be completed later in Settings.`,
+  newline: `Step 3 of 3\n\nTV Guide reloaded.`,
+  entity: `To Start Watching&#58;&hellip;`,
+  strong: `<strong>Kindig Customs</strong> Dave buys a 1929 Roadster that he wants to fix up and flip, but Kevin fears he's bought another lemon; Kris tackles an engine that won't cooperate.`,
+  inline: `Parental level set to <span style='color: #F0F0F0'>{0}</span> between <span style='color: #F0F0F0'>{1}</span>.`,
+  simple: `<a class="show-link show-link--catchup" href="/program/kindig-customs/322906/" data-event-id="117580179">
+<h4>Kindig Customs</h4>
+<p>10:30 PM</p>
+</a>`,
+  structured: `<div class="show-down__content">
+<p class="show-down__tags">Reality/Real-Life | USA | 2016 | PG</p><a role="button" href="" class="js-close-show-down show-down__close" data-tracking-link-name="grid_showdown_close">1</a>
+<h2>Kindig Customs</h2>
+<h3>It's Like 1929 In Here</h3>
+<div class="show-down__info">
+    <p class="show-down__timeFromTo">10:30PM - 11:30PM</p>
+    <p class="show-down__closedCaptions hidden">CC</p>
+    <p class="show-down__repeat">REPEAT</p>
+</div>
+<p class="show-down__description">Dave buys a 1929 Roadster that he wants to fix up and flip, but Kevin fears he's bought another lemon; Kris tackles an engine that won't cooperate.</p>
+</p>
+</div>`
+};
