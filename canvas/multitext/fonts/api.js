f52d4c028ac6699f716e17a777ac147a6d56d88e
Sharkie <craig.sharkie@gmail.com>
2021-01-21 17:18:01 +1100

    C5N Canvas.multitext()
    Tweaking the **fonts.api** in the fervent hope that the overlooked font file will be found
    
    [NOTE] Git is really quite broken here :(
    
    - Doing the needful

--- canvas/multitext/fonts/api.js
+++ canvas/multitext/fonts/api.js
@@ -1,5 +1,17 @@
-window.fonts = [
-  { family: 'Texta Light', url: '/fonts/texta-light.ttf' },
-  { family: 'Texta Medium', url: '/fonts/texta-medium.ttf' },
-  { family: 'Texta Heavy', url: '/fonts/texta-heavy.ttf' }
+window.fonts = [{
+    family: 'Texta Light',
+    url: '/fonts/texta-light.ttf'
+  },
+  {
+    family: 'Texta Medium',
+    url: '/fonts/texta-medium.ttf'
+  },
+  {
+    family: 'Texta Heavy',
+    url: '/fonts/texta-heavy.ttf'
+  },
+  {
+    family: 'Texta Thin',
+    url: '/fonts/texta-thin.ttf'
+  }
 ];
