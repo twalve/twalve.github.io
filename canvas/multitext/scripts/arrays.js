7619603d4a728165e53e256618ee630e94fb2796
Sharkie <craig.sharkie@gmail.com>
2021-01-29 12:13:32 +1100

    C5N Canvas.multitext()
    Add a range of images to test the `renderImage()` method
    
    - Add and populate **assets** to reserve images
    - Add and populate **images/avatars** to serve images
    - Add and populate **images/buttons** to serve images
    - Add and populate **images/gauges** to serve images

---
+++ canvas/multitext/scripts/arrays.js
@@ -0,0 +1,50 @@
+const arrays = {
+  bold: [
+    "<ftx>Point your remote at the TV and press",
+    "<strong>Volume +",
+    "</strong>"
+  ],
+  bolder: [
+    "<ftx>Point your remote at the TV and press",
+    "<strong>Volume +",
+    "</strong>",
+    "for louder sound"
+  ],
+  guage: [
+    "<ftx>Gauge image",
+    "<img src=\"/canvas/multitext/images/gauges/Monotone@2x.png\" height=\"24\" width=\"36\" />",
+    " inlined in text ",
+    "<br />",
+    "<br />I got in one little fight and my mom got scared. She said \"You're movin' with your aunty and uncle in Bel Air\"."
+  ],
+  icon: [
+    "<ftx>Green",
+    "<icon src=\"/canvas/multitext/images/buttons/buttonGreen.png\" />",
+    " for Yes | Red",
+    "<icon src=\"/canvas/multitext/images/buttons/buttonRed.png\" />",
+    " for No"
+  ],
+  image: [
+    "<ftx>User: ",
+    "<img src=\"/canvas/multitext/images/avatars/koala.png\" height=\"42\" width=\"42\" />",
+    " Blinky"
+  ],
+  linebreak: [
+    "<ftx>Step 3 of 3",
+    "<br />TV Guide reloaded."
+  ],
+  multi: [
+    "<ftx>Don't Worry.",
+    "<br />Pairing can be completed later in Settings."
+  ],
+  newline: [
+    "<ftx>Step 3 of 3",
+    "<br />",
+    "<br />TV Guide reloaded."
+  ],
+  paragraph: [
+    "<ftx>In West Philadelphia born and raised on the playground was where I spent most of my days. Chillin' out, maxin', relaxin', all cool and all shootin' some b-ball outside of the school. When a couple of guys who were up to no good, started making trouble in my neighborhood.",
+    "<br />",
+    "<br />I got in one little fight and my mom got scared. She said \"You're movin' with your aunty and uncle in Bel Air\"."
+  ]
+};
